c870ba92c686ee2782e614b43b06eb3d06f0c57f
Rabeka Branham <rabeka.june@gmail.com>
2023-10-12 14:05:38 -0500

    updating exercises with further information & code

--- db_tables_exercises.sql
+++ db_tables_exercises.sql
@@ -15,6 +15,12 @@ SHOW tables; -- List all tables in the database
 DESCRIBE employees; -- Explore the employees table. What different data types are present in this table? 
 -- ANSWER: int, date, varchar(14), varchar(16), enum('M','F')
 
+DESCRIBE departments;
+DESCRIBE dept_emp;
+DESCRIBE dept_manager;
+DESCRIBE employees;
+DESCRIBE salaries;
+DESCRIBE titles;
 -- Which table(s) do you think contain a numeric type column?
 -- ANSWER: dept_emp, dept_manager, employees, salaries, titles
 
@@ -24,7 +30,19 @@ DESCRIBE employees; -- Explore the employees table. What different data types ar
 -- Which table(s) do you think contain a date type column?
 -- ANSWER: dept_emp, dept_manager, employees, salaries, titles
 
+DESCRIBE employees;
+DESCRIBE departments;
 -- What is the relationship between the employees and the departments tables?
 -- ANSWER: No relationship is present
 
 SHOW CREATE TABLE dept_manager;-- Show the SQL code that created the dept_manager table. Write the SQL it takes to show this as your exercise solution.
+-- CREATE TABLE `dept_manager` (
+--   `emp_no` int NOT NULL,
+--   `dept_no` char(4) NOT NULL,
+--   `from_date` date NOT NULL,
+--   `to_date` date NOT NULL,
+--   PRIMARY KEY (`emp_no`,`dept_no`),
+--   KEY `dept_no` (`dept_no`),
+--   CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE ON UPDATE RESTRICT,
+--   CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE ON UPDATE RESTRICT
+-- ) ENGINE=InnoDB DEFAULT CHARSET=latin1
