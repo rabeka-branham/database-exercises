cb42002512f5c87f2344edc6b0523bae39db896e
Rabeka Branham <rabeka.june@gmail.com>
2023-10-13 09:22:12 -0500

    update code to multiple lines

--- basic_statement_exercises.sql
+++ basic_statement_exercises.sql
@@ -8,27 +8,38 @@ DESCRIBE albums;
 -- What is the primary key for the albums table?
 -- ANSWER: id
 
-SELECT name FROM albums;
+SELECT name 
+FROM albums;
 -- What does the column named 'name' represent?
 -- ANSWER: the title of the album.
 
-SELECT sales FROM albums;
+SELECT sales 
+FROM albums;
 -- What do you think the sales column represents?
 -- ANSWER: # of album sales in millions
 
-SELECT name FROM albums WHERE artist = 'Pink Floyd';
+SELECT name 
+FROM albums 
+WHERE artist = 'Pink Floyd';
 -- Find the name of all albums by Pink Floyd.
 -- ANSWER: The Dark Side of the Moon & The Wall
 
-SELECT release_date FROM albums WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
+SELECT release_date 
+FROM albums 
+WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
 -- What is the year Sgt. Pepper's Lonely Hearts Club Band was released?
 -- ANSWER: 1967
 
-SELECT genre FROM albums WHERE name = 'Nevermind';
+SELECT genre 
+FROM albums 
+WHERE name = 'Nevermind';
 -- What is the genre for the album Nevermind?
 -- ANSWER: Grunge, Alternative rock
 
-SELECT name FROM albums WHERE release_date >= 1990 and release_date <= 1999;
+SELECT name 
+FROM albums 
+WHERE release_date >= 1990 
+	AND release_date <= 1999;
 -- Which albums were released in the 1990s?
 /* ANSWER: 
 The Bodyguard
@@ -43,7 +54,9 @@ Metallica
 Nevermind
 Supernatural*/
 
-SELECT name as low_selling_albums FROM albums WHERE sales < 20;
+SELECT name as low_selling_albums 
+FROM albums 
+WHERE sales < 20;
 -- Which albums had less than 20 million certified sales? Rename this column as low_selling_albums.
 /* ANSWER:
 Grease: The Original Soundtrack from the Motion Picture
