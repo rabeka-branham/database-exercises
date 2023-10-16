25a082d475c24c742e3c2240e3315e3f34afb674
Rabeka Branham <rabeka.june@gmail.com>
2023-10-16 09:12:39 -0500

    saving & ready for submission

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -17,7 +17,7 @@ WHERE last_name LIKE 'E%E';
 -- Convert the names produced in your last query to all uppercase.
 
 SELECT 
-	count(concat(first_name, ' ', last_name)))
+	count(concat(first_name, ' ', last_name))
 FROM employees
 WHERE last_name LIKE 'E%E';
 -- Use a function to determine how many results were returned from your previous query.
@@ -25,6 +25,7 @@ WHERE last_name LIKE 'E%E';
 SELECT 
 	first_name,
     last_name,
+    hire_date,
     datediff(now(), hire_date) days_since_hired
 FROM employees
 WHERE hire_date LIKE '199%'
@@ -37,7 +38,6 @@ SELECT
 FROM salaries;
 -- Find the smallest and largest current salary from the salaries table.
 
-select * from employees;
 SELECT
 	lower(
 		concat(
@@ -52,6 +52,3 @@ SELECT
     birth_date
 FROM employees;
 -- Use your knowledge of built in SQL functions to generate a username for all of the employees. A username should be all lowercase, and consist of the first character of the employees first name, the first 4 characters of the employees last name, an underscore, the month the employee was born, and the last two digits of the year that they were born. Below is an example of what the first 10 rows will look like:
-
-
-
