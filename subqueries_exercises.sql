a1c1195522ebfcd86573149b04a0c24835e34750
Rabeka Branham <rabeka.june@gmail.com>
2023-10-19 10:19:21 -0500

    uploading after review

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -10,42 +10,29 @@ select * from salaries;
 select * from titles;
 -- Create a file named subqueries_exercises.sql and craft queries to return the results for the following criteria:
         
-SELECT concat(first_name, ' ', last_name)
-FROM employees
+SELECT *
+FROM employees e
+	JOIN dept_emp de
+    ON e.emp_no = de.emp_no
+    AND de.to_date > now()
 WHERE hire_date = (
 	SELECT hire_date
 	FROM employees
 	WHERE emp_no = 101010);
 -- 1. Find all the current employees with the same hire date as employee 101010 using a subquery.
 
-SELECT 
-	t.title,
-    count(*)
-FROM titles t
-	JOIN dept_emp de
-		ON de.emp_no = t.emp_no
-        AND de.to_date > NOW()
-	JOIN (
-			SELECT *
-			FROM employees
-			WHERE first_name IN ('Aamod')
-			) aamod
-        ON aamod.emp_no = de.emp_no
-GROUP BY t.title;
+SELECT DISTINCT title
+FROM titles
+WHERE emp_no IN 
+	(
+	SELECT emp_no
+	FROM employees
+	WHERE first_name = 'Aamod'
+    )
+    AND to_date > now();
+-- 2. Find all the titles ever held by all current employees with the first name Aamod.
 
-SELECT t.title,
-	count(*)
-FROM employees e
-	JOIN dept_emp de
-		ON e.emp_no = de.emp_no
-        AND e.first_name IN ('aamod')
-        AND de.to_date = 99990101
-	JOIN titles t
-		ON t.emp_no = de.emp_no
-GROUP BY t.title;
--- Find all the titles ever held by all current employees with the first name Aamod.
-
-SELECT count(*)
+SELECT *
 FROM employees e
 	JOIN
 			(
@@ -57,7 +44,17 @@ FROM employees e
             ) as de
 		ON de.emp_no = e.emp_no
 		AND de.to_date < now();
--- How many people in the employees table are no longer working for the company? Give the answer in a comment in your code.
+        
+-- REVIEW CODE:
+SELECT count(*)
+FROM employees 
+WHERE emp_no NOT IN
+	(
+    SELECT emp_no
+    FROM dept_emp
+    WHERE to_date > now()
+    );
+-- 3. How many people in the employees table are no longer working for the company? Give the answer in a comment in your code.
 -- ANSWER: 59900
 
 SELECT concat(f.first_name, ' ', f.last_name)
@@ -70,22 +67,32 @@ FROM
 		JOIN dept_manager dm
 			ON dm.emp_no = f.emp_no
 			AND dm.to_date > now();
--- Find all the current department managers that are female. List their names in a comment in your code.
+            
+-- REVIEW CODE:
+SELECT *
+FROM employees
+WHERE gender = 'F'
+	AND emp_no IN
+		(
+        SELECT emp_no
+		FROM dept_manager
+		WHERE to_date > now()
+        );
+-- 4. Find all the current department managers that are female. List their names in a comment in your code.
 -- ANSWER: Isamu Legleitner, Karsten Sigstam, Leon DasSarma, & Hilary Kambil
 
-SELECT *
-FROM employees e
-	JOIN salaries s
-    ON e.emp_no = s.emp_no
-    AND s.to_date > now()
-WHERE s.salary > 
-	(
-	SELECT AVG(salary)
-	FROM salaries
-    );   
--- Find all the employees who currently have a higher salary than the companie's overall, historical average salary.
+SELECT count(*)
+FROM salaries 
+WHERE 
+	to_date > now()
+    AND salary > 
+		(
+		SELECT AVG(salary)
+		FROM salaries
+		);
+-- 5. Find all the employees who currently have a higher salary than the companie's overall, historical average salary.
 
-SELECT count(salary) -- 83
+SELECT count(*) -- 83
 FROM salaries 
 WHERE 
 	to_date > now() 
@@ -95,9 +102,9 @@ WHERE
 		FROM salaries
 		WHERE to_date > now()
 		);
-
-SELECT CONCAT((
-	(SELECT count(salary)
+	
+SELECT CONCAT(( -- .0346%
+	(SELECT count(*)
 	FROM salaries 
 	WHERE 
 		to_date > now()
